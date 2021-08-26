593decce2b940eedde3a493052352b7f271f0610
Tom Quellenberg <quelle@subshell.com>
2021-08-26 22:49:38 +0200

    Show full day in chart

--- chart/chart.go
+++ chart/chart.go
@@ -2,6 +2,7 @@ package chart
 
 import (
 	"log"
+	"time"
 
 	"net/http"
 
@@ -25,11 +26,16 @@ func generateLineItems(entries []datastore.Entry) []opts.LineData {
 func lineChart(name string, data []datastore.Entry) *charts.Line {
 	// create a new line instance
 	line := charts.NewLine()
+
+	now := time.Now()
+	year, month, day := now.Date()
+	start := time.Date(year, month, day, 0, 0, 0, 0, now.Location()).Format(datastore.DateTimeFormat)
+	end := time.Date(year, month, day, 23, 59, 55, 0, now.Location()).Format(datastore.DateTimeFormat)
 	// set some global options like Title/Legend/ToolTip or anything else
 	line.SetGlobalOptions(
 		charts.WithInitializationOpts(opts.Initialization{Theme: types.ThemeWonderland, Height: "300px"}),
 		charts.WithTitleOpts(opts.Title{Title: name}),
-		charts.WithXAxisOpts(opts.XAxis{Type: "time", SplitNumber: 12}),
+		charts.WithXAxisOpts(opts.XAxis{Type: "time", Min: start, Max: end, SplitNumber: 12}),
 		charts.WithYAxisOpts(opts.YAxis{Min: "dataMin", Max: "dataMax"}, 0))
 	// Put data into instance
 	sunrise, sunset := sun.GetDayInfo()
