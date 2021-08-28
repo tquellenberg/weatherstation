c623e661d3e0defc4de15d189d28a51b09d75c2b
Tom Quellenberg <quelle@subshell.com>
2021-08-28 21:50:32 +0200

    Code cleanup

--- chart/index.go
+++ chart/index.go
@@ -24,20 +24,28 @@ type JsonDataEntry struct {
 	Value []interface{} `json:"value"`
 }
 
+type dataFunc func(start, end time.Time) ([]datastore.Entry, error)
+
+type XRange int
+
+const (
+	Day XRange = iota
+	Week
+)
+
 func getTimeRange(req *http.Request) (start, end time.Time) {
-	param1 := req.URL.Query().Get("range")
-	xRange := 0
-	switch param1 {
+	var xRange XRange
+	switch req.URL.Query().Get("range") {
 	case "week":
-		xRange = 1
+		xRange = Week
 	default:
-		xRange = 0
+		xRange = Day
 	}
 
 	now := time.Now()
 	year, month, day := now.Date()
 	xstart := time.Date(year, month, day, 0, 0, 0, 0, now.Location())
-	if xRange == 1 {
+	if xRange == Week {
 		xstart = xstart.AddDate(0, 0, -6)
 	}
 	xend := time.Date(year, month, day, 23, 59, 55, 0, now.Location())
@@ -45,45 +53,33 @@ func getTimeRange(req *http.Request) (start, end time.Time) {
 }
 
 func TempData(w http.ResponseWriter, req *http.Request) {
-	xstart, xend := getTimeRange(req)
-
-	tData, _ := datastore.GetTemperatureSeries(xstart, xend)
-	jsonData := make([]JsonDataEntry, 0, len(tData))
-	for _, v := range tData {
-		jsonData = append(jsonData, JsonDataEntry{Value: []interface{}{v.Time, v.Value}})
-	}
-
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(jsonData)
+	jsonData(w, req, datastore.GetTemperatureSeries)
 }
 
 func PressureData(w http.ResponseWriter, req *http.Request) {
-	xstart, xend := getTimeRange(req)
-
-	tData, _ := datastore.GetPressureSeries(xstart, xend)
-	jsonData := make([]JsonDataEntry, 0, len(tData))
-	for _, v := range tData {
-		jsonData = append(jsonData, JsonDataEntry{Value: []interface{}{v.Time, v.Value}})
-	}
-
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(jsonData)
+	jsonData(w, req, datastore.GetPressureSeries)
 }
 
 func HumidityData(w http.ResponseWriter, req *http.Request) {
+	jsonData(w, req, datastore.GetHumiditySeries)
+}
+
+func jsonData(w http.ResponseWriter, req *http.Request, dataFunc dataFunc) {
 	xstart, xend := getTimeRange(req)
+	tData, err := dataFunc(xstart, xend)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	} else {
+		jsonData := make([]JsonDataEntry, 0, len(tData))
+		for _, v := range tData {
+			jsonData = append(jsonData, JsonDataEntry{Value: []interface{}{v.Time, v.Value}})
+		}
 
-	tData, _ := datastore.GetHumiditySeries(xstart, xend)
-	jsonData := make([]JsonDataEntry, 0, len(tData))
-	for _, v := range tData {
-		jsonData = append(jsonData, JsonDataEntry{Value: []interface{}{v.Time, v.Value}})
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(jsonData)
 	}
-
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(jsonData)
 }
 
 func Index(w http.ResponseWriter, req *http.Request) {
